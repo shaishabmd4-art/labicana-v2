53cf8ae401877d548ce1210bbee234ef9204903c
shaishabmd4-art <shaishabmd4@gmail.com>
2026-07-23 01:55:30 +0200

    Initialize Firebase in firebase.js
    Removed React rendering code and added Firebase initialization.

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const firebaseConfig = {
+  apiKey: 'AIzaSyBlm8q8YHV0L-LMJyBl3S1d6x4sgn-aNa8',
+  authDomain: 'labicana-ce02f.firebaseapp.com',
+  projectId: 'labicana-ce02f',
+  storageBucket: 'labicana-ce02f.firebasestorage.app',
+  messagingSenderId: '1078482645531',
+  appId: '1:1078482645531:web:1d45200789637cde0d3ebe',
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
